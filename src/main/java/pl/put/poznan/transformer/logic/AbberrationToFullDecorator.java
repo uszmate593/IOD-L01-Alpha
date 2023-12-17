8bcbca21fe161d71e110936010c5f9ab2c9e8275
uszmate593 <95314247+uszmate593@users.noreply.github.com>
2023-12-17 13:19:12 +0100

    Expanding abberration to full dictionary for #9

--- src/main/java/pl/put/poznan/transformer/logic/AbberrationToFullDecorator.java
+++ src/main/java/pl/put/poznan/transformer/logic/AbberrationToFullDecorator.java
@@ -17,8 +17,7 @@ public class AbberrationToFullDecorator extends AbstractDecorator
     public String decorate()
     {
         String[] words = super.decorate().split( " ");
-        Map<String, String> abbreviations = new HashMap<String, String>();
-        abbreviations.put("PUT", "Poznan University of Technology");
+        Map<String, String> abbreviations = loadAbberrationDictionary();
 
         for(int i = 0;i < words.length; i++)
             if(abbreviations.containsKey(words[i]))
@@ -26,5 +25,33 @@ public class AbberrationToFullDecorator extends AbstractDecorator
 
         String result = String.join(" ", words);
         return result;
+    }
+
+    private Map<String, String> loadAbberrationDictionary()
+    {
+        Map<String, String> dictionary = new HashMap<String, String>();
+        dictionary.put("PUT", "Poznan University of Technology");
+        dictionary.put("itd.", "i tak dalej");
+        dictionary.put("np.", "na przykład");
+        dictionary.put("prof.", "profesor");
+        dictionary.put("Prof.", "Profesor");
+        dictionary.put("dr", "doktor");
+        dictionary.put("Dr", "Doktor");
+        dictionary.put("mgr", "magister");
+        dictionary.put("Mgr", "Magister");
+        dictionary.put("itp.", "i tym podobne");
+        dictionary.put("wg", "według");
+        dictionary.put("Wg", "Według");
+        dictionary.put("al.", "aleja");
+        dictionary.put("Al.", "Aleja");
+        dictionary.put("ul.", "ulica");
+        dictionary.put("Ul.", "Ulica");
+        dictionary.put("lek.", "lekarz");
+        dictionary.put("NIP", "Numer Identyfikacji Podatkowej");
+        dictionary.put("PESEL", "Powszechny Elektroniczny System Ewidencji Ludności");
+        dictionary.put("RP", "Rzeczpospolita Polska");
+        dictionary.put("p.o.", "pełniący obowiązki");
+
+        return dictionary;
     }
 }
