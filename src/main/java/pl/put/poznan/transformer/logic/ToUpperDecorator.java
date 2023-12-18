e5e7af69d155eb454879d394d591f21d0c1d9c1b
Morelowo <147094545+wojtekchwacinski@users.noreply.github.com>
2023-12-18 16:25:19 +0100

    Creating javadoc for #15
    Javadoc dla ToLowerDecorator,
    ToCapitalizeDecorator, ToUpperDecorator

--- src/main/java/pl/put/poznan/transformer/logic/ToUpperDecorator.java
+++ src/main/java/pl/put/poznan/transformer/logic/ToUpperDecorator.java
@@ -1,10 +1,27 @@
 package pl.put.poznan.transformer.logic;
-
+/**
+ * Klasa odpowiadająca za zamienię wszytskich liter w tekscie na duże
+ *
+ * @author Mikołaj Lewandowski
+ *
+ */
 public class ToUpperDecorator extends AbstractDecorator{
+
+    /**
+     * Konstruktor klasy, przyjmujący jeden argument.
+     *
+     * @param text Konstruktor klasy przyjmuje obiekt klasy TextImplementation, który chcemy przekształcić.
+     */
     public ToUpperDecorator(Text text){
         super(text);
     }
 
+
+    /**
+     * Metoda zamieniająca oryginalny tekst podany w konstruktorze na wielkie litery.
+     *
+     * @return Zwraca tekst po zamianie na wielkie litery jako obiekt typu String.
+     */
     @Override
     public String decorate(){
         return(super.decorate().toUpperCase());
